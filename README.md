dd72791288d4beac2eb7ad4a73516de963b5d025
Shaurya Tiwari <ltsmartofficial@gmail.com>
2024-08-26 16:38:29 +0530

    Add files via upload
    my loving confession

---
+++ README.md
@@ -0,0 +1,2 @@
+# Confession
+# shaurya tiwari
---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+   titleWeb: 'my love 🤍⁉️',
+   introTitle: 'Baby I want To confess my LOVE? 💌⁉️',
+   introDesc: `This will be a quick confession.
+    Mujhe itna batao mere bare me kya feel krti ho,
+    now you have to follow my heart baby ishke alawaa koi option bhi nhi hai .
+    Follow the instructions carefully sahi se krna samjhi.😝😍`,
+   btnIntro: `😚 yaha click kro😍`,
+   title: 'You know, I’ve had a crush on you ever since I saw you. Maybe this is what they call love at first sight 😄',
+   desc: 'Do You Also Like ME???? 😚😱',
+   btnYes: 'YES!!!! 😄',
+   btnNo: 'NO??? 😱',
+   question: `What do you like about me? I don't have a lot of talents, looks, skills, or communication skills, I only have my pure and loyal heart . 💖😱`,
+   btnReply: 'I Love You.',
+   reply: `I Love The Way You Are............HAHAHA🤣🤣🤣`,
+   mess: `Mai toh tumhare saath maje le rha tha baby you know how much i love you 😚😚😚😚😚😚😚😚😚😚 🤭💖 `,
+   messDesc: 'Thank You For The Participation Love you baby , ab neeche love you to pr click kr ke tum bhi love you bol do 😝⁉❗️ ',
+   btnAccept: 'Love you too babu 😽🥰️⁉️',
+   messLink: 'https://tinyurl.com/3y8ceh5p',
+};
